67f8dc1b9771c70ae3da3460957c45b70f07f48b
nnnocturn <vikchehovich@gmail.com>
2020-11-22 19:45:35 +0200

    all p

--- js/service.js
+++ js/service.js
@@ -9,3 +9,11 @@ document.querySelectorAll('.service').forEach(function(elm){
 document.getElementsByClassName("pizzochka")[0].addEventListener("click", function(){
     window.scrollTo(0, document.querySelector(".cart--area").offsetTop);
 });
+
+document.getElementsByClassName("oury")[0].addEventListener("click", function(){
+    window.scrollTo(0, document.querySelector(".pizza-area").offsetTop);
+});
+
+document.getElementsByClassName("acti")[0].addEventListener("click", function(){
+    window.scrollTo(0, document.querySelector(".Stocks").offsetTop);
+});
